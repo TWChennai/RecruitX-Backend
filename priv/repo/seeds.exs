2777309dd850fa71f5a57fe825b8775470d21a88
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-01-15 18:35:13 +0530

    Seeding for candidates.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,9 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+import Ecto.Query
+
+alias RecruitxBackend.Candidate
 alias RecruitxBackend.Repo
 alias RecruitxBackend.Role
 alias RecruitxBackend.Skill
@@ -31,9 +34,21 @@ Enum.map(["Java",
 end)
 
 Enum.map(%{"Code Pairing" => 1,
-            "Technical1" => 2,
-            "Technical2" => 3,
-            "Leadership" => 4,
-            "P3" => 4}, fn {name_value, priority_value} ->
+           "Technical1" => 2,
+           "Technical2" => 3,
+           "Leadership" => 4,
+           "P3" => 4}, fn {name_value, priority_value} ->
   Repo.insert!(%Interview{name: name_value, priority: priority_value})
+end)
+
+role_ids = Repo.all(from role in Role, select: role.id)
+Enum.map(["Dinesh",
+          "Kausalya",
+          "Maha",
+          "Navaneetha",
+          "Pranjal",
+          "Sivasubramanian",
+          "Subha",
+          "Vijay"], fn name_value ->
+  Repo.insert!(%Candidate{name: name_value, experience: Decimal.new(Float.round(:random.uniform * 10, 2)), role_id: Enum.random(role_ids)})
 end)
