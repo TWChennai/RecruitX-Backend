48cf68c4bc5c2e7aa2de95c3d2e457c938a36e86
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-02-01 12:38:46 +0530

    Vijay: Upgrade npm modules and also removed some deprecated usages from Phoeix.

--- web/web.ex
+++ web/web.ex
@@ -18,8 +18,9 @@ defmodule RecruitxBackend.Web do
 
   def model do
     quote do
-      use Ecto.Model
+      use Ecto.Schema
 
+      import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
     end
@@ -30,7 +31,7 @@ defmodule RecruitxBackend.Web do
       use Phoenix.Controller
 
       alias RecruitxBackend.Repo
-      import Ecto.Model
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
 
       import RecruitxBackend.Router.Helpers
@@ -48,7 +49,7 @@ defmodule RecruitxBackend.Web do
       use Phoenix.Channel
 
       alias RecruitxBackend.Repo
-      import Ecto.Model
+      import Ecto
       import Ecto.Query, only: [from: 1, from: 2]
     end
   end
