d76e35423b5db39b8e8a0e618daf30a114b1b2db
Subha <subhamanoharanbe@gmail.com>
2016-02-03 17:31:42 +0530

    Subha: Fixing get /candidates/id enpoint which was failing due to common interview json

--- web/views/interview_view.ex
+++ web/views/interview_view.ex
@@ -2,7 +2,7 @@ defmodule RecruitxBackend.InterviewView do
   use RecruitxBackend.Web, :view
 
   def render("index.json", %{interviews: interviews}) do
-    render_many(interviews, RecruitxBackend.InterviewView, "interview.json")
+    render_many(interviews, RecruitxBackend.InterviewView, "interview_with_signup.json")
   end
 
   def render("show.json", %{interview: interview}) do
@@ -16,13 +16,22 @@ defmodule RecruitxBackend.InterviewView do
     }
   end
 
-  def render("interview.json", %{interview: interview}) do
+  def render("interview_with_signup.json", %{interview: interview}) do
     %{
       id: interview.id,
       start_time: interview.start_time,
       candidate: render_one(interview.candidate, RecruitxBackend.CandidateView, "candidate.json"),
       interview_type: render_one(interview.interview_type, RecruitxBackend.InterviewTypeView, "interview_type_without_id.json"),
-      sign_up: interview.signup
+      signup: interview.signup
+    }
+  end
+
+  def render("interview.json", %{interview: interview}) do
+    %{
+      id: interview.id,
+      start_time: interview.start_time,
+      candidate: render_one(interview.candidate, RecruitxBackend.CandidateView, "candidate.json"),
+      interview_type: render_one(interview.interview_type, RecruitxBackend.InterviewTypeView, "interview_type_without_id.json")
     }
   end
 end
