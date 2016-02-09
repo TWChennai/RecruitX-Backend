d89a3e402ffbb5c6abb9b4623253439676dfe5cc
Subha <subhamanoharanbe@gmail.com>
2016-02-09 09:52:21 +0530

    Subha: Adding integration tests for getting interviews that a panelist has taken

---
+++ spec/integration/interview_integration_spec.exs
@@ -0,0 +1,42 @@
+defmodule RecruitxBackend.InterviewIntegrationSpec do
+  use ESpec.Phoenix, controller: RecruitxBackend.InterviewController
+
+  alias RecruitxBackend.Repo
+  alias RecruitxBackend.Candidate
+  alias RecruitxBackend.CandidateSkill
+  alias RecruitxBackend.Interview
+  alias RecruitxBackend.InterviewPanelist
+
+  @moduletag :integration
+  @endpoint RecruitxBackend.Endpoint
+
+  describe "index" do
+    before do: Repo.delete_all(Interview)
+    before do: Repo.delete_all(InterviewPanelist)
+    before do: Repo.delete_all(CandidateSkill)
+    before do: Repo.delete_all(Candidate)
+
+    it "should return empty when panelist has taken no interviews" do
+      interview = create(:interview)
+
+      response = get conn(), "/panelists/recruitx/interviews"
+
+      expect(response.status) |> to(be(200))
+      expect(response.assigns.interviews) |> to(be([]))
+    end
+
+    it "should return list of interviews that the panelist has taken" do
+      candidate = create(:candidate)
+      interview = create(:interview, candidate_id: candidate.id, candidate: candidate)
+      create(:candidate_skill, candidate_id: candidate.id, candidate: candidate)
+      create(:interview_panelist, panelist_login_name: "test", interview_id: interview.id)
+
+      response = get conn(), "/panelists/test/interviews"
+
+      expect(response.status) |> to(be(200))
+      [result_interview] = response.assigns.interviews
+      expect(compare_fields(result_interview, interview, [:id, :start_time])) |> to(be_true)
+      expect(compare_fields(result_interview.candidate, interview.candidate, [:name, :experience, :role_id, :other_skills])) |> to(be_true)
+    end
+  end
+end
--- spec/support/test_helpers.ex
+++ spec/support/test_helpers.ex
@@ -2,4 +2,8 @@ defmodule RecruitxBackend.TestHelpers do
   def convertKeysFromAtomsToStrings(input) do
     for {key, val} <- input, into: %{}, do: {to_string(key), val}
   end
+
+  def compare_fields( map1, map2, fields) do
+    Enum.all?(fields, fn(field) -> Map.get(map1, field) == Map.get(map2, field) end)
+  end
 end
