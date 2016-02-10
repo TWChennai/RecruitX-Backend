62287ff7065bea87a3d0d4a9a795831e93db2132
Subha <subhamanoharanbe@gmail.com>
2016-02-10 16:56:39 +0530

    Subha: Adding interview_id and time in secs to file name

--- web/controllers/FeedbackController.ex
+++ web/controllers/FeedbackController.ex
@@ -2,25 +2,22 @@ defmodule RecruitxBackend.FeedbackController do
   use RecruitxBackend.Web, :controller
 
   alias RecruitxBackend.Endpoint
+  alias Timex.Date
 
-  def create(conn, params) do
+  def create(conn, %{"avatar" => data, "interview_id" => id}) do
     path = Endpoint.config(:path_to_store_images)
-    %{"avatar" => data, "interview_id" => id } = params
-    if File.exists?(path) and File.dir?(path) do
-      File.cp!(data.path, path <> "/trial1.jpg")
-    else
-      File.mkdir!(path)
-      File.cp!(data.path, path <> "/trial1.jpg")
-    end
-    conn |> put_status(200) |> json("")
+    new_file_path = path <> "/interview_#{id}_#{Date.now(:secs)}" <> ".jpg"
+    if !(File.exists?(path) and File.dir?(path)), do: File.mkdir!(path)
+    File.cp!(data.path, new_file_path)
+    conn |> put_status(200) |> json("File uploaded!")
   end
 
   def index(conn, params) do
-     path = Endpoint.config(:path_to_store_images)
-     if File.exists?(path) and File.dir?(path) do
-       send_file(conn, 200, path <> "/trial1.jpg", 0, :all)
-     else
+    path = Endpoint.config(:path_to_store_images)
+    if File.exists?(path) and File.dir?(path) do
+      send_file(conn, 200, path <> "/trial1.jpg", 0, :all)
+    else
       conn |> put_status(200) |> json("file not found")
-     end
+    end
   end
 end
