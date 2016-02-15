e0156361668303071a1ba471564ba49c3796d2b3
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-02-15 18:46:34 +0530

    Vijay: Load the system env vars only once and store as constants.

--- web/controllers/jigsaw_controller.ex
+++ web/controllers/jigsaw_controller.ex
@@ -1,15 +1,13 @@
 defmodule RecruitxBackend.JigsawController do
   use RecruitxBackend.Web, :controller
 
-  alias RecruitxBackend.Endpoint
-
   @recruitment_department "People Recruiting"
   @invalid_user "not a valid user"
+  @jigsaw_url System.get_env("JIGSAW_URL")
+  @token System.get_env("JIGSAW_TOKEN")
 
   def show(conn, %{"id" => id}) do
-    jigsaw_url =  System.get_env("JIGSAW_URL")
-    token = System.get_env("JIGSAW_TOKEN")
-    response = HTTPotion.get jigsaw_url <> id, [headers: ["Authorization": token]]
+    response = HTTPotion.get("#{@jigsaw_url}#{id}", [headers: ["Authorization": @token]])
     is_recruiter = case response.body do
       "" -> @invalid_user
       _  -> case response.body |> Poison.Parser.parse do
@@ -18,7 +16,7 @@ defmodule RecruitxBackend.JigsawController do
                             department_key_value_pair = for {key, val} <- department, into: %{}, do: {String.to_atom(key), val}
                             case department_key_value_pair.name do
                               @recruitment_department -> true
-                              _otherDepartments -> false
+                              _ -> false
                             end
             {:error, reason} -> reason
             end
