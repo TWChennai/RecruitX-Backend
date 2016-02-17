8314bb95cf3adae9a93f38c9e715d973961f780d
Maha Lakshme <mahalaks@thoughtworks.com>
2016-02-17 14:58:05 +0530

    #91:Maha:For testing simulated two users, one in recruitment and other not in recruitment

--- web/controllers/jigsaw_controller.ex
+++ web/controllers/jigsaw_controller.ex
@@ -9,20 +9,26 @@ defmodule RecruitxBackend.JigsawController do
   @token System.get_env("JIGSAW_TOKEN")
 
   def show(conn, %{"id" => id}) do
-    response = HTTPotion.get("#{@jigsaw_url}#{id}", [headers: ["Authorization": @token]])
-    is_recruiter = case response.body do
-      "" -> @invalid_user
-      _  -> case response.body |> Parser.parse do
+      case id do
+        "mahalakshme1994" -> is_recruiter = false
+         conn |> render("show.json", is_recruiter: is_recruiter)
+         "shridhar.u.p" -> is_recruiter = true
+         conn |> render("show.json", is_recruiter: is_recruiter)
+         _  -> response = HTTPotion.get("#{@jigsaw_url}#{id}", [headers: ["Authorization": @token]])
+        is_recruiter = case response.body do
+          "" -> @invalid_user
+          _  -> case response.body |> Parser.parse do
             {:ok, body} ->  key_value_response = for {key, val} <- body, into: %{}, do: {String.to_atom(key), val}
-                            department = key_value_response.department
-                            department_key_value_pair = for {key, val} <- department, into: %{}, do: {String.to_atom(key), val}
+                        department = key_value_response.department
+                        department_key_value_pair = for {key, val} <- department, into: %{}, do: {String.to_atom(key), val}
                             case department_key_value_pair.name do
                               @recruitment_department -> true
                               _ -> false
                             end
-            {:error, reason} -> reason
-            end
+           {:error, reason} -> reason
+        end
     end
     conn |> render("show.json", is_recruiter: is_recruiter)
   end
+end
 end
