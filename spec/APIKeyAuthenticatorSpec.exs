e0b7990be6d2eca25152272fbd4743cbe40ef8a0
Subha <subhamanoharanbe@gmail.com>
2016-02-29 18:41:55 +0530

    Adding logs for debugging

--- spec/APIKeyAuthenticatorSpec.exs
+++ spec/APIKeyAuthenticatorSpec.exs
@@ -6,6 +6,8 @@ defmodule RecruitxBackend.APIKeyAuthenticatorSpec do
 
   alias RecruitxBackend.APIKeyAuthenticator
 
+  require Logger
+
   describe "api key authentication" do
     it "should return 401 and not process request if authorization header is not present" do
       response = APIKeyAuthenticator.call(conn(), :empty)
@@ -17,7 +19,6 @@ defmodule RecruitxBackend.APIKeyAuthenticatorSpec do
     it "should return 401 and not process request if API key is invalid" do
       conn = put_req_header(conn(), "authorization", "invalid")
       response = APIKeyAuthenticator.call(conn, :empty)
-
       expect(response.status) |> to(be(401))
       expect(response.resp_body) |> to(be("Invalid API key"))
     end
--- web/APIKeyAuthenticator.ex
+++ web/APIKeyAuthenticator.ex
@@ -7,8 +7,11 @@ defmodule RecruitxBackend.APIKeyAuthenticator do
     options
   end
 
+  require Logger
   def call(conn, _opts) do
     api_key_header = List.first(get_req_header(conn, "authorization"))
+    Logger.info("System API Key: #{@api_key}")
+    Logger.info("REquest header: #{api_key_header}")
     if @api_key == api_key_header, do: conn , else: conn |> send_resp(:unauthorized, "Invalid API key") |> halt
   end
 end
