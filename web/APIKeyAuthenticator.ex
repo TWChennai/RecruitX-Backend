b99f64b5c0d3392f1cb2f4d2fd809de27bfecc6c
Arunvel Sriram <arunvelsriram@gmail.com>
2016-03-02 15:32:05 +0530

    Arun: #DeployTask: Always rebuild to recompile stale dependencies

--- web/APIKeyAuthenticator.ex
+++ web/APIKeyAuthenticator.ex
@@ -12,6 +12,6 @@ defmodule RecruitxBackend.APIKeyAuthenticator do
     api_key_header = List.first(get_req_header(conn, "authorization"))
     Logger.info("System API Key: #{@api_key}")
     Logger.info("Request header: #{api_key_header}")
-    if @api_key == api_key_header, do: conn , else: conn |> send_resp(:unauthorized, "Invalid API key") |> halt
+    if @api_key == api_key_header, do: conn, else: conn |> send_resp(:unauthorized, "Invalid API key") |> halt
   end
 end
