01daeb4103569697bfb537b25dd2d811832c2ddd
Arunvel Sriram <arunvelsriram@gmail.com>
2016-03-02 15:46:04 +0530

    Arun: #DeployTask: Swapping logs for forcing recompilation

--- web/APIKeyAuthenticator.ex
+++ web/APIKeyAuthenticator.ex
@@ -10,8 +10,8 @@ defmodule RecruitxBackend.APIKeyAuthenticator do
   require Logger
   def call(conn, _opts) do
     api_key_header = List.first(get_req_header(conn, "authorization"))
-    Logger.info("System API Key: #{@api_key}")
     Logger.info("Request header: #{api_key_header}")
+    Logger.info("System API Key: #{@api_key}")
     if @api_key == api_key_header, do: conn, else: conn |> send_resp(:unauthorized, "Invalid API key") |> halt
   end
 end
