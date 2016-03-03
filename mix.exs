954f34ccae9655ec1454eb703acdb208f5666e1b
Arunvel Sriram <arunvelsriram@gmail.com>
2016-03-03 11:50:04 +0530

    Arun: Updating Elixir version to 1.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RecruitxBackend.Mixfile do
   def project do
     [app: :recruitx_backend,
      version: "1.0.0",
-     elixir: "~> 1.2.0",
+     elixir: "~> 1.2.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
