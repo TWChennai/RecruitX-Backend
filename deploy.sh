30f2d196e7ba7212236326dac23d934052d431b9
Arunvel Sriram <arunvelsriram@gmail.com>
2016-03-03 10:41:53 +0530

    Arun: #DeployTask: Updated deploy script, Added env variables

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 
-# From: https://github.com/HashNuke/ansible-elixir-stack
-export MIX_ENV=prod
-ansible-playbook playbooks/migrate.yml
-ansible-playbook playbooks/deploy.yml
+if [ -n "$1" -a -n "$2" ]
+then
+  git ls-remote --exit-code $1 &> /dev/null
+  if [ $? -ne 0 ]
+  then
+    # create remote if not exists
+    git remote add $1 $2
+    echo "Added remote - $1"
+  fi
+  # deploy and migrate
+  git push $1 master
+  heroku run mix ecto.migrate --remote $1
+else
+  echo "Usage: ./deploy.sh remote_name URL"
+fi
