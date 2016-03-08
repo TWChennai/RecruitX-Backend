e1807526f73457b2d1211efa2dd4d4a400f2a322
Subha <subhamanoharanbe@gmail.com>
2016-03-08 13:11:19 +0530

    Subha/Sukanya: Combining sign up validations

--- web/models/interview_panelist.ex
+++ web/models/interview_panelist.ex
@@ -46,32 +46,34 @@ defmodule RecruitxBackend.InterviewPanelist do
     model
     |> cast(params, @required_fields, @optional_fields)
     |> validate_format(:panelist_login_name, AppConstants.name_format)
-    |> validate_signup_count(params)
     |> validate_sign_up_for_interview(params)
     |> unique_constraint(:panelist_login_name, name: :interview_panelist_login_name_index, message: "You have already signed up for this interview")
     |> assoc_constraint(:interview, message: "Interview does not exist")
   end
 
   #TODO:'You have already signed up for the same interview' constraint error never occurs as it is handled here at changeset level itself
-  defp validate_sign_up_for_interview(existing_changeset, %{candidate_ids_interviewed: candidate_ids_interviewed, my_previous_sign_up_start_times: my_previous_sign_up_start_times, interview: interview})
-    when not(is_nil(candidate_ids_interviewed)) and not(is_nil(my_previous_sign_up_start_times)) and not(is_nil(interview)) do
+  defp validate_sign_up_for_interview(existing_changeset, %{candidate_ids_interviewed: candidate_ids_interviewed, my_previous_sign_up_start_times: my_previous_sign_up_start_times, interview: interview, signup_counts: signup_counts})
+    when not(is_nil(candidate_ids_interviewed)) and not(is_nil(my_previous_sign_up_start_times)) and not(is_nil(interview))  and not(is_nil(signup_counts)) do
       existing_changeset
       |> validate_panelist_has_not_interviewed_candidate(candidate_ids_interviewed, interview)
       |> validate_panelist_has_no_other_interview_within_time_buffer(interview, my_previous_sign_up_start_times)
       |> validate_interview_is_not_over(interview)
+      |> validate_signup_count(signup_counts, interview.id)
   end
 
-  defp validate_sign_up_for_interview(existing_changeset, params) do
+  defp validate_sign_up_for_interview(existing_changeset, _) do
     interview_id = get_field(existing_changeset, :interview_id)
     panelist_login_name = get_field(existing_changeset, :panelist_login_name)
     if check_not_nil([interview_id, panelist_login_name]) do
       interview = Interview |> Repo.get(interview_id)
       if !is_nil(interview) do
         {candidate_ids_interviewed, my_sign_up_start_times} = get_candidate_ids_and_start_times_interviewed_by(panelist_login_name)
+        signup_counts = __MODULE__.get_interview_type_based_count_of_sign_ups |> where(interview_id: ^interview.id) |> Repo.all
         existing_changeset = validate_sign_up_for_interview(existing_changeset, %{panelist_login_name: panelist_login_name,
           candidate_ids_interviewed: candidate_ids_interviewed,
           my_previous_sign_up_start_times: my_sign_up_start_times,
-          interview: interview})
+          interview: interview,
+          signup_counts: signup_counts})
       end
     end
     existing_changeset
@@ -92,13 +94,8 @@ defmodule RecruitxBackend.InterviewPanelist do
     existing_changeset
   end
 
-  defp validate_signup_count(existing_changeset, params) do
-    id = get_field(existing_changeset, :interview_id)
-    if !is_nil(id) do
-      get_counts = __MODULE__.get_interview_type_based_count_of_sign_ups
-      signup_counts = if is_nil(params[:signup_counts]), do: get_counts |> where(interview_id: ^id) |> Repo.all, else: params[:signup_counts]
-      if !Interview.is_signup_lesser_than_max_count(id, signup_counts), do: existing_changeset = add_error(existing_changeset, :signup_count, "More than #{@max_count} signups are not allowed")
-    end
+  defp validate_signup_count(existing_changeset, signup_counts, interview_id) do
+    if existing_changeset.valid? and !Interview.is_signup_lesser_than_max_count(interview_id, signup_counts), do: existing_changeset = add_error(existing_changeset, :signup_count, "More than #{@max_count} signups are not allowed")
     existing_changeset
   end
 
