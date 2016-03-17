983082c183532648c6d632c166e8ab76452aa10f
Subha <subhamanoharanbe@gmail.com>
2016-03-17 12:55:43 +0530

    Subha/Sukanya: Inserted the filters in experience matrix

--- priv/repo/migrations/20160314090724_create_experience_matrix.exs
+++ priv/repo/migrations/20160314090724_create_experience_matrix.exs
@@ -3,8 +3,8 @@ defmodule RecruitxBackend.Repo.Migrations.CreateExperienceMatrix do
 
   def change do
     create table(:experience_matrices) do
-      add :panelist_experience_lower_bound, :integer, null: false
-      add :candidate_experience_upper_bound, :integer, null: false
+      add :panelist_experience_lower_bound, :decimal, null: false, precision: 4, scale: 2
+      add :candidate_experience_upper_bound, :decimal, null: false, precision: 4, scale: 2
       add :interview_type_id, references(:interview_types, on_delete: :delete_all), null: false
 
       timestamps
