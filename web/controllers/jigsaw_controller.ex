376027cca26904b48da346ee2ce4abb12825f024
Dinesh Balasubramanian <dineshudt17 - at - gmail - dot - com>
2016-03-17 18:24:03 +0530

    Dinesh: Refactoring create new year_to_month method

--- web/controllers/jigsaw_controller.ex
+++ web/controllers/jigsaw_controller.ex
@@ -2,51 +2,57 @@ defmodule RecruitxBackend.JigsawController do
   use RecruitxBackend.Web, :controller
 
   alias Poison.Parser
+  alias Timex.Date
 
   @recruitment_department "People Recruiting"
   @invalid_user "not a valid user"
   @jigsaw_url System.get_env("JIGSAW_URL")
   @token System.get_env("JIGSAW_TOKEN")
 
-  @lint {Credo.Check.Refactor.CyclomaticComplexity, false}
+  @lint [{Credo.Check.Refactor.ABCSize, false}, {Credo.Check.Refactor.CyclomaticComplexity, false}]
   def show(conn, %{"id" => id}) do
     user_details = case id do
-      "ppanelist" -> %{is_recruiter: false, calculated_hire_date: Timex.Date.now |> Timex.Date.shift(months: -10), past_experience: 0.24}
-      "ppanelistp" -> %{is_recruiter: false, calculated_hire_date: Timex.Date.now |> Timex.Date.shift(months: -100), past_experience: 0.24}
-      "rrecruitx" -> %{is_recruiter: true, calculated_hire_date: Timex.Date.now |> Timex.Date.shift(months: -10), past_experience: 0.24}
-      "rrecruitxr" -> %{is_recruiter: true, calculated_hire_date: Timex.Date.now |> Timex.Date.shift(months: -100), past_experience: 0.24}
+      "ppanelist" -> %{is_recruiter: false, calculated_hire_date: Date.now |> Date.shift(months: -10), past_experience: 0.24}
+      "ppanelistp" -> %{is_recruiter: false, calculated_hire_date: Date.now |> Date.shift(months: -100), past_experience: 0.24}
+      "rrecruitx" -> %{is_recruiter: true, calculated_hire_date: Date.now |> Date.shift(months: -10), past_experience: 0.24}
+      "rrecruitxr" -> %{is_recruiter: true, calculated_hire_date: Date.now |> Date.shift(months: -100), past_experience: 0.24}
       _  -> response = HTTPotion.get("#{@jigsaw_url}#{id}", [headers: ["Authorization": @token]])
         case response.body do
-          "" -> @invalid_user
+          "" -> %{is_recruiter: @invalid_user, calculated_hire_date: Date.now, past_experience: 0}
           _  -> case response.body |> Parser.parse do
                   {:ok, body} -> department = body["department"]
                                  tw_experience = body["twExperience"]
                                  total_experience = body["totalExperience"]
                                  past_experience = Decimal.new(total_experience - tw_experience)
                                                    |> Decimal.round(2)
-                                 {tw_year_experience, tw_months_experience} = tw_experience
-                                                                                      |> Decimal.new
-                                                                                      |> Decimal.to_string
-                                                                                      |> Integer.parse
-                                 tw_experience_in_months = tw_year_experience * 12
-                                                        |> Decimal.new
-                                                        |> Decimal.add(tw_months_experience
-                                                                        |> Decimal.new
-                                                                        |> Decimal.mult(Decimal.new(12)))
-                                 {tw_experience_in_integer , _} = tw_experience_in_months
-                                                                        |> Decimal.to_string
-                                                                        |> Integer.parse
-                                 calculated_hire_date = Timex.Date.now
-                                                        |> Timex.Date.shift(months: -tw_experience_in_integer)
+                                 tw_experience_in_month = tw_experience |> year_to_month
+                                 calculated_hire_date = Date.now
+                                                        |> Date.shift(months: -tw_experience_in_month)
                   case department["name"] do
                     @recruitment_department -> %{is_recruiter: true, calculated_hire_date: calculated_hire_date, past_experience: past_experience}
                     _ -> %{is_recruiter: false, calculated_hire_date: calculated_hire_date, past_experience: past_experience}
                   end
-                  {:error, reason} -> reason
+                  {:error, reason} -> %{is_recruiter: reason, calculated_hire_date: Date.now, past_experience: 0}
                 end
-      end
+        end
     end
 
     conn |> render("show.json", user_details: user_details)
+  end
+
+  defp year_to_month(experience) do
+    {year_experience, months_experience} = experience
+                                           |> Decimal.new
+                                           |> Decimal.to_string
+                                           |> Integer.parse
+    experience_in_months = year_experience * 12
+                           |> Decimal.new
+                           |> Decimal.add(months_experience
+                                           |> Decimal.new
+                                           |> Decimal.mult(Decimal.new(12)))
+    {experience_in_integer, _} = experience_in_months
+                                  |> Decimal.to_string
+                                  |> Integer.parse
+    experience_in_integer
   end
 end
--- web/views/jigsaw_view.ex
+++ web/views/jigsaw_view.ex
@@ -1,10 +1,12 @@
 defmodule RecruitxBackend.JigsawView do
   use RecruitxBackend.Web, :view
 
+  alias Timex.DateFormat
+
   def render("show.json", %{user_details: user_details}) do
     %{
       is_recruiter: user_details.is_recruiter,
-      calculated_hire_date: Timex.DateFormat.format!(user_details.calculated_hire_date, "%Y-%m-%d", :strftime),
+      calculated_hire_date: DateFormat.format!(user_details.calculated_hire_date, "%Y-%m-%d", :strftime),
       past_experience: user_details.past_experience
     }
   end
