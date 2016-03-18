fe068e12843a56b86dc56689cceb5e1f71bcb1c2
Subha <subhamanoharanbe@gmail.com>
2016-03-18 10:13:07 +0530

    Subha: Adding experience for testing accounts

--- web/controllers/jigsaw_controller.ex
+++ web/controllers/jigsaw_controller.ex
@@ -11,11 +11,12 @@ defmodule RecruitxBackend.JigsawController do
 
   @lint [{Credo.Check.Refactor.ABCSize, false}, {Credo.Check.Refactor.CyclomaticComplexity, false}]
   def show(conn, %{"id" => id}) do
+    {experience, id} = parse_experience(id)
     user_details = case id do
-      "ppanelist" -> %{is_recruiter: false, calculated_hire_date: Date.now |> Date.shift(months: -10), past_experience: 0.24}
-      "ppanelistp" -> %{is_recruiter: false, calculated_hire_date: Date.now |> Date.shift(months: -100), past_experience: 0.24}
-      "rrecruitx" -> %{is_recruiter: true, calculated_hire_date: Date.now |> Date.shift(months: -10), past_experience: 0.24}
-      "rrecruitxr" -> %{is_recruiter: true, calculated_hire_date: Date.now |> Date.shift(months: -100), past_experience: 0.24}
+      "ppanelist" -> %{is_recruiter: false, calculated_hire_date: Date.now |> Date.shift(months: -12), past_experience: experience}
+      "ppanelistp" -> %{is_recruiter: false, calculated_hire_date: Date.now |> Date.shift(months: -18), past_experience: experience}
+      "rrecruitx" -> %{is_recruiter: true, calculated_hire_date: Date.now |> Date.shift(months: -12), past_experience: experience}
+      "rrecruitxr" -> %{is_recruiter: true, calculated_hire_date: Date.now |> Date.shift(months: -18), past_experience: experience}
       _  -> response = HTTPotion.get("#{@jigsaw_url}#{id}", [headers: ["Authorization": @token]])
         case response.body do
           "" -> %{is_recruiter: @invalid_user, calculated_hire_date: Date.now, past_experience: 0}
@@ -38,6 +39,11 @@ defmodule RecruitxBackend.JigsawController do
     end
 
     conn |> render("show.json", user_details: user_details)
+  end
+
+  defp parse_experience(id) do
+    result = Float.parse(id)
+    if result != :error, do: result, else: {1.5, id}
   end
 
   defp year_to_month(experience) do
