e6e67c4f528b409708be0f001837866c299c9b0d
Subha <subhamanoharanbe@gmail.com>
2016-03-22 10:17:05 +0530

    Subha: Add priority

--- priv/repo/migrations/20160321081417_add_interview_types_for_qa.exs
+++ priv/repo/migrations/20160321081417_add_interview_types_for_qa.exs
@@ -6,7 +6,7 @@ defmodule RecruitxBackend.Repo.Migrations.AddInterviewTypesForQa do
 
   def change do
     Enum.each([InterviewType.telephonic], fn interview_type_value ->
-      Repo.insert!(%InterviewType{name: interview_type_value})
+      Repo.insert!(%InterviewType{name: interview_type_value, priority: 1})
     end)
   end
 end
