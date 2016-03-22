78e2f1b9b250a96a13caf0e03896a11f11df21fd
Arunvel Sriram <arunvelsriram@gmail.com>
2016-03-22 11:50:17 +0530

    Arun: Fix typo

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,4 @@
 import Ecto.Query
 
 alias RecruitxBackend.{Repo, Interview, InterviewPanelist, InterviewType, Candidate, Skill, Role, CandidateSkill, ExperienceMatrix, FeedbackImage, InterviewStatus, PipelineStatus, RoleSkill, WeeklySignupReminder}
-alias Timex.{Date, DateTim, DateTimeFormatter}
+alias Timex.{Date, DateTime, DateTimeFormatter}
