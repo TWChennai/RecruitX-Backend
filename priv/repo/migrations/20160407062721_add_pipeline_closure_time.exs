ff31e885071613926b55b95f534606de3f659abf
pvaishna <pvaishna@INpvaishna.local>
2016-04-07 12:43:09 +0530

    Kausalya/Vaish: created migration to include pipeline closure time

---
+++ priv/repo/migrations/20160407062721_add_pipeline_closure_time.exs
@@ -0,0 +1,22 @@
+defmodule RecruitxBackend.Repo.Migrations.AddPipelineClosureTime do
+ use Ecto.Migration
+
+ alias RecruitxBackend.PipelineStatus
+ alias RecruitxBackend.Candidate
+
+ import Ecto.Query
+
+ def change do
+   alter table(:candidates) do
+     add :pipeline_closure_time, :datetime
+   end
+
+ closed_pipeline_status_id = PipelineStatus.retrieve_by_name(PipelineStatus.closed).id
+
+ flush
+
+ # closed_candidates = Repo.all(from c in Candidate, where: c.pipeline_status_id == ^closed_pipeline_status_id)
+ execute "UPDATE candidates SET pipeline_closure_time = candidates.updated_at WHERE pipeline_status_id = #{closed_pipeline_status_id}"
+ end
+
+end
