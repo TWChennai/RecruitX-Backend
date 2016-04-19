7623a1b3326796b1488acda5e82d7a09bb0e55ba
Dinesh Balasubramanian <dineshudt17 - at - gmail - dot - com>
2016-04-19 11:55:04 +0530

    Dinesh/Vaish: #Refactoring to find the end of week

--- web/helpers/previous_week.ex
+++ web/helpers/previous_week.ex
@@ -3,6 +3,6 @@ defmodule RecruitxBackend.PreviousWeek do
   alias Timex.Date
 
   def get do
-    %{starting: Date.beginning_of_week(Date.now), ending: Date.beginning_of_week(Date.now) |> Date.shift(days: 4) |> Date.end_of_day }
+    %{starting: Date.beginning_of_week(Date.now), ending: Date.end_of_week(Date.now) |> Date.shift(days: -2)}
   end
 end
