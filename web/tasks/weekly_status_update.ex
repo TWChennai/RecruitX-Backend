ee774e32fcc4f99c9351e3ce1dd8b6e1606a2692
Subha <subhamanoharanbe@gmail.com>
2016-04-19 11:41:22 +0530

    Subha/Priya: Removing previous week as constants

--- web/tasks/weekly_status_update.ex
+++ web/tasks/weekly_status_update.ex
@@ -9,8 +9,6 @@ defmodule RecruitxBackend.WeeklyStatusUpdate do
   alias RecruitxBackend.Repo
   alias Timex.DateFormat
 
-  @previous_week PreviousWeek.get
-
   def execute do
     query = Interview |> Interview.working_days_in_current_week |> preload([:interview_panelist, :interview_status, :interview_type])
     candidates_weekly_status = Candidate
@@ -20,9 +18,10 @@ defmodule RecruitxBackend.WeeklyStatusUpdate do
     candidates = candidates_weekly_status
                   |> filter_out_candidates_without_interviews
                   |> construct_view_data
-    summary = candidates |> construct_summary_data
-    {:ok, start_date} = @previous_week.starting |> DateFormat.format("{D}/{M}/{YY}")
-    {:ok, to_date} = @previous_week.ending |> DateFormat.format("{D}/{M}/{YY}")
+    previous_week = PreviousWeek.get
+    summary = candidates |> construct_summary_data(previous_week)
+    {:ok, start_date} = previous_week.starting |> DateFormat.format("{D}/{M}/{YY}")
+    {:ok, to_date} = previous_week.ending |> DateFormat.format("{D}/{M}/{YY}")
     email_content = if candidates != [], do: Templates.weekly_status_update(start_date, to_date, candidates, summary),
                     else: Templates.weekly_status_update_default(start_date, to_date)
     Mailer.deliver(%{
@@ -41,19 +40,19 @@ defmodule RecruitxBackend.WeeklyStatusUpdate do
     end)
   end
 
-  defp construct_summary_data(candidates) do
-    {candidates_pursued, candidates_rejected} = Candidate.get_candidates_pursued_and_rejected_after_pipeline_closure_separately(@previous_week)
+  defp construct_summary_data(candidates, previous_week) do
+    {candidates_pursued, candidates_rejected} = Candidate.get_candidates_pursued_and_rejected_after_pipeline_closure_separately(previous_week)
     %{
       candidates_appeared: Enum.count(candidates),
       interviews_count: candidates |> get_total_no_of_interviews,
       candidates_in_progress: Candidate.get_total_no_of_candidates_in_progress,
       candidates_pursued: Enum.count(candidates_pursued),
-      candidates_rejected: get_total_no_of_rejects(candidates_rejected)
+      candidates_rejected: get_total_no_of_rejects(candidates_rejected, previous_week)
     }
   end
 
-  defp get_total_no_of_rejects(candidates_rejected) do
-    Candidate.get_no_of_pass_candidates_within_range(@previous_week) + Enum.count(candidates_rejected)
+  defp get_total_no_of_rejects(candidates_rejected, previous_week) do
+    Candidate.get_no_of_pass_candidates_within_range(previous_week) + Enum.count(candidates_rejected)
   end
 
   defp get_total_no_of_interviews(candidates) do
