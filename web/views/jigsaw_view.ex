c0c125a4318c8cebf8728fdade2397e644e6a980
priyadarshini <priyadr@thoughtworks.com>
2016-05-09 17:56:25 +0530

    Priya/Subha: added is_super_user method based for office principle

--- web/views/jigsaw_view.ex
+++ web/views/jigsaw_view.ex
@@ -9,7 +9,8 @@ defmodule RecruitxBackend.JigsawView do
       is_recruiter: user_details.is_recruiter,
       calculated_hire_date: DateFormat.format!(user_details.calculated_hire_date, "%Y-%m-%d", :strftime),
       past_experience: user_details.past_experience,
-      role: render_one(user_details.role, RoleView, "role_without_skills.json")
+      role: render_one(user_details.role, RoleView, "role_without_skills.json"),
+      is_super_user: user_details.is_super_user
     }
   end
 end
