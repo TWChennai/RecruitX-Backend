9221f245cb2e03a0ac003d5d4cc213494a0698e3
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-06-13 13:07:46 +0530

    Priya/Sarath: Added candidate's name onto the webpage

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule RecruitxBackend.PageController do
   use RecruitxBackend.Web, :controller
 
+alias RecruitxBackend.Candidate
+
   def index(conn, _params) do
-    render conn, "index.html"
+    candidates =  candidates = Candidate.get_candidates_in_fifo_order
+              |> Repo.all
+
+    render(conn, "index.html", candidates: candidates)
   end
 end
