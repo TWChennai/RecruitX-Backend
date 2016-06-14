dc75503024398e1b6e07d199dbd7fc399c160828
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-06-14 20:27:14 +0530

    Vijay: Adding TODO

--- web/controllers/interview_controller.ex
+++ web/controllers/interview_controller.ex
@@ -15,7 +15,7 @@ defmodule RecruitxBackend.InterviewController do
 
   def index(conn, %{"panelist_login_name" => panelist_login_name, "panelist_experience" => panelist_experience,  "panelist_role" => panelist_role}) do
     interviews = Interview.get_interviews_with_associated_data
-                  |> preload([:interview_type, candidate: :role])
+                  |> preload([:interview_type, candidate: :role]) # TODO: This line is not needed in case the request being served is json, only needed for html web version - please optimize
                   |> Panel.now_or_in_next_seven_days
                   |> Panel.default_order
                   |> Repo.all
