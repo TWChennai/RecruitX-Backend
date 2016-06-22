11363d617c187fc1039f3abfaf725b476321cddc
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-06-22 15:16:17 +0530

    Sarath/Priya: Added cookies when login by Okta Widget

--- web/controllers/login_controller.ex
+++ web/controllers/login_controller.ex
@@ -2,8 +2,9 @@ defmodule RecruitxBackend.LoginController do
   use RecruitxBackend.Web, :controller
 
   @okta_preview System.get_env("OKTA_PREVIEW")
+  @api_key System.get_env("API_KEY")
 
   def index(conn, _params) do
-    if @okta_preview, do: render(conn,"index.html", okta_preview: @okta_preview), else: conn |> send_resp(:not_found, "Invalid Okta Url") |> halt
+    if @okta_preview, do: render(conn,"index.html", okta_preview: @okta_preview, api_key: @api_key), else: conn |> send_resp(:not_found, "Invalid Okta Url") |> halt
   end
 end
