7e2aa94b136337e21306b9ca212a0da408e4b28a
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-06-24 19:46:05 +0530

    Priya: wrote test when it should not redirect to login page

--- spec/integration/interview_integration_spec.exs
+++ spec/integration/interview_integration_spec.exs
@@ -116,5 +116,16 @@ defmodule RecruitxBackend.InterviewIntegrationSpec do
       expect(response.status) |> to(be(302))
       expect(response.path_info) |> to(be(["web"]))
     end
+
+    it "should not redirect if there is cookie" do
+      conn = Plug.Conn.put_resp_cookie(conn, "calculated_hire_date", "dummy_date")
+      conn = Plug.Conn.put_resp_cookie(conn, "panelist_role", "Dev")
+      conn = Plug.Conn.put_resp_cookie(conn, "username", "dummy_user")
+
+      response = get conn, "/web/"
+
+      expect(response.status) |> to(be(200))
+      expect(response.path_info) |> to(be(["web"]))
+    end
   end
 end
