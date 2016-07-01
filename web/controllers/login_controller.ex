efb4ad4321977b1b15b478a30e358b6784c0a34e
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-07-01 11:36:06 +0530

    Priya: Stored Okta session

--- web/controllers/login_controller.ex
+++ web/controllers/login_controller.ex
@@ -2,7 +2,7 @@ defmodule RecruitxBackend.LoginController do
   use RecruitxBackend.Web, :controller
 
   @okta_preview System.get_env("OKTA_PREVIEW")
-  @api_key System.get_env("API_KEY")
+  @api_url System.get_env("API_URL")
 
   alias RecruitxBackend.JigsawController
 
@@ -13,6 +13,6 @@ defmodule RecruitxBackend.LoginController do
   end
 
   def index(conn, _params) do
-    if @okta_preview, do: render(conn,"index.html", okta_preview: @okta_preview, api_key: @api_key), else: conn |> send_resp(:not_found, "Invalid Okta Url") |> halt
+    if @okta_preview, do: render(conn,"index.html", okta_preview: @okta_preview, api_url: @api_url), else: conn |> send_resp(:not_found, "Invalid Okta Url") |> halt
   end
 end
