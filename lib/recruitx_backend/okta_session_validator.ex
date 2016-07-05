008864554f88b7766f83e5f42190f910cff90158
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-07-05 17:53:36 +0530

    Ganesh/Priya: Added experience and modified session validator

--- lib/recruitx_backend/okta_session_validator.ex
+++ lib/recruitx_backend/okta_session_validator.ex
@@ -14,9 +14,12 @@ defmodule RecruitxBackend.OktaSessionValidator do
     conn = fetch_cookies(conn)
     session_id = conn.cookies["okta_session_id"]
     username = conn.cookies["username"]
+    if !session_id || !username do
+      conn |> Phoenix.Controller.redirect(to: "/login")
+    end
     if !conn.cookies["calculated_hire_date"] || !conn.cookies["panelist_role"] do
       %{user_details: user_details} = JigsawController.get_jigsaw_data(username)
-      conn = Plug.Conn.put_resp_cookie(conn, "calculated_hire_date", user_details.calculated_hire_date)
+      conn = Plug.Conn.put_resp_cookie(conn, "calculated_hire_date", "2015-06-05")
       conn = Plug.Conn.put_resp_cookie(conn, "panelist_role", user_details.role.name)
     end
     response = HTTPotion.get(@okta_preview <> "/api/v1/sessions/" <> session_id, [headers: ["Authorization": @okta_api_key]])
