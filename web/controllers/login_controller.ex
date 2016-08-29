f073c6fda5ef14b342eb35be59771ad05e453613
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-08-29 23:04:22 +0530

    [Priya] Caught User does not exist

--- web/controllers/login_controller.ex
+++ web/controllers/login_controller.ex
@@ -5,12 +5,19 @@ defmodule RecruitxBackend.LoginController do
   alias Timex.DateFormat
   alias Plug.Conn
 
+  @invalid_user "not a valid user"
+
   def index(conn = %Plug.Conn{cookies: %{"username" => username}}, _params) do
+
     %{user_details: user_details} = JigsawController.get_jigsaw_data(username)
-    hire_date = DateFormat.format!(user_details.calculated_hire_date, "%Y-%m-%d", :strftime)
-    conn = Conn.put_resp_cookie(conn, "calculated_hire_date", hire_date, http_only: false)
-    conn = Conn.put_resp_cookie(conn, "panelist_role", user_details.role.name, http_only: false)
-    conn |> redirect(to: "/homepage")
+    if user_details.is_recruiter != @invalid_user do
+      hire_date = DateFormat.format!(user_details.calculated_hire_date, "%Y-%m-%d", :strftime)
+      conn = Conn.put_resp_cookie(conn, "calculated_hire_date", hire_date, http_only: false)
+      conn = Conn.put_resp_cookie(conn, "panelist_role", user_details.role.name, http_only: false)
+      conn |> redirect(to: "/homepage")
+    else
+      conn |> render("index.html",error: "Invalid Username!")
+    end
   end
 
   def index(conn, _params) do
