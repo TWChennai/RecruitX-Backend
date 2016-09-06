66825b9bb43256cf2ae9cb49e38c7470ce6ae5f8
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-09-06 20:06:41 +0530

    [Priya] Checked username should not contain just numbers

--- web/static/js/entry.js
+++ web/static/js/entry.js
@@ -1,9 +1,15 @@
 export var Entry = {
   continue: function() {
     var username = document.getElementById("username").value;
+    var isnum = /^\d+$/.test(username);
+    var error_element = document.getElementById("error")
+    if(isnum)
+    {
+      error_element.innerHTML = "Username cannot be a number!";
+      return
+    }
     if(!username){
-      var error_element = document.getElementById("error")
-      error_element.innerHTML = "Username Cannot be Empty";
+      error_element.innerHTML = "Username Cannot be Empty!";
       return
     }
     document.cookie = "username=" + username;
