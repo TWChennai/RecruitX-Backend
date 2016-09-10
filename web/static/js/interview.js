c0beec20cfa754a2dd5bde1e22bd72e4015ca0b1
Subha <subhamanoharanbe@gmail.com>
2016-09-11 01:38:35 +0530

    [Subha] Styling sign up dialog for interviews

--- web/static/js/interview.js
+++ web/static/js/interview.js
@@ -1,27 +1,35 @@
 export var Interview = {
+  cancel: function() {
+    var error_element = document.getElementById("signupError");
+    error_element.innerHTML = "";
+    error_element.className = "";
+  },
   signup: function(id, api_key) {
+    var error_element = document.getElementById("signupError");
     var panelist_experience = this.calculate_years_difference(new Date(), new Date($.cookie("calculated_hire_date")));
-    if(confirm("Are you sure, you want to signup?")) {
-      $.ajax({
-        url: "/panelists",
-        method: 'POST',
-        data: JSON.stringify({
-         "interview_panelist": {
-           "interview_id": id,
-           "panelist_login_name": $.cookie("username"),
-           "panelist_experience": panelist_experience,
-           "panelist_role": $.cookie("panelist_role")
-         }
-       }),
-        success: function(response) {
-          window.location = '/my_interviews';
-        },
-        headers: {
-          "Authorization": api_key,
-          "Content-Type": "application/json"
-        }
-      });
-    }
+    $.ajax({
+      url: "/panelists",
+      method: 'POST',
+      data: JSON.stringify({
+       "interview_panelist": {
+         "interview_id": id,
+         "panelist_login_name": $.cookie("username"),
+         "panelist_experience": panelist_experience,
+         "panelist_role": $.cookie("panelist_role")
+       }
+     }),
+      success: function(response) {
+        window.location = '/my_interviews';
+      },
+      headers: {
+        "Authorization": api_key,
+        "Content-Type": "application/json"
+      },
+      error: function(error) {
+        error_element.innerHTML = "Something went wrong! Please try again later!";
+        error_element.className += "alert alert-danger";
+      },
+    });
   },
   calculate_years_difference: function(date1, date2) {
     return Math.round(Math.abs(date1.getTime() - date2.getTime()) /  (1000 * 3600 * 24 * 365));
