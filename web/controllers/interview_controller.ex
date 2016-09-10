21803a0a151889b4be4a99830178232ee99fb5af
Priyadarshini Ravi <priyadharshiniravi03@gmail.com>
2016-09-10 16:41:48 +0530

    [Priya] : Sorted interviews in my interviews and corrected need signup in all interviews

--- web/controllers/interview_controller.ex
+++ web/controllers/interview_controller.ex
@@ -70,6 +70,7 @@ defmodule RecruitxBackend.InterviewController do
     panelist_experience = Date.diff((calculated_hire_date |> DateFormat.parse!("%Y-%m-%d", :strftime)), Date.now, :years)
     retrieved_panelist_role = Role.retrieve_by_name(panelist_role)
     interviews_and_slots_with_signup_status = Panel.add_signup_eligibity_for(slots, interviews, panelist_login_name, panelist_experience, retrieved_panelist_role)
+                                                |> Enum.sort(fn (first, second) -> first.signup || !second.signup end)
     conn |> render("index.html", interviews_with_signup: interviews_and_slots_with_signup_status, api_key: @api_key, not_login: true)
   end
 
