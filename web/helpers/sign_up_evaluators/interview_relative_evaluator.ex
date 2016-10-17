53efe623bec9cf9edee1d859886321f46ba2c6ce
Dinesh Balasubramanian <dineshudt17@gmail.com>
2016-10-17 23:46:17 +0530

    [Dinesh] Refactoring Moved the valid checks to method level

--- web/helpers/sign_up_evaluators/interview_relative_evaluator.ex
+++ web/helpers/sign_up_evaluators/interview_relative_evaluator.ex
@@ -14,34 +14,36 @@ defmodule RecruitxBackend.InterviewRelativeEvaluator do
     |> is_signup_count_lesser_than_max(sign_up_data_container.signup_counts, sign_up_data_container.interview_type_based_sign_up_limits, interview, sign_up_data_container.slot)
   end
 
+  defp has_not_interviewed_candidate(%{valid?: false} = sign_up_evaluation_status, _, _, _), do: sign_up_evaluation_status
   defp has_not_interviewed_candidate(sign_up_evaluation_status, _, _, true), do: sign_up_evaluation_status
-
   defp has_not_interviewed_candidate(sign_up_evaluation_status, candidate_ids_interviewed, interview, false) do
-    if sign_up_evaluation_status.valid? and !has_panelist_not_interviewed_candidate(interview, candidate_ids_interviewed) do
+    if !has_panelist_not_interviewed_candidate(interview, candidate_ids_interviewed) do
       sign_up_evaluation_status = sign_up_evaluation_status |> SignUpEvaluationStatus.add_errors({:signup, "You have already signed up an interview for this candidate"})
     end
     sign_up_evaluation_status
   end
 
+  defp has_no_other_interview_within_time_buffer(%{valid?: false} = sign_up_evaluation_status, _, _), do: sign_up_evaluation_status
   defp has_no_other_interview_within_time_buffer(sign_up_evaluation_status, my_previous_sign_up_start_times, interview) do
-    if sign_up_evaluation_status.valid? and !is_within_time_buffer_of_my_previous_sign_ups(interview, my_previous_sign_up_start_times) do
+    if !is_within_time_buffer_of_my_previous_sign_ups(interview, my_previous_sign_up_start_times) do
       sign_up_evaluation_status = sign_up_evaluation_status |> SignUpEvaluationStatus.add_errors({:signup, "You are already signed up for another interview within #{@time_buffer_between_sign_ups} hours"})
     end
     sign_up_evaluation_status
   end
 
+  defp is_interview_not_over(%{valid?: false} = sign_up_evaluation_status, _, _), do: sign_up_evaluation_status
   defp is_interview_not_over(sign_up_evaluation_status, _, true), do: sign_up_evaluation_status
-
   defp is_interview_not_over(sign_up_evaluation_status, interview, false) do
-    if sign_up_evaluation_status.valid? and !Interview.is_not_completed(interview) do
+    if !Interview.is_not_completed(interview) do
       sign_up_evaluation_status = sign_up_evaluation_status |> SignUpEvaluationStatus.add_errors({:signup, "Interview is already over!"})
     end
     sign_up_evaluation_status
   end
 
+  defp is_signup_count_lesser_than_max(%{valid?: false} = sign_up_evaluation_status, _, _, _, _), do: sign_up_evaluation_status
   defp is_signup_count_lesser_than_max(sign_up_evaluation_status, signup_counts, interview_type_based_sign_up_limits, interview, is_slot) do
     max_sign_up_limit = Enum.find_value(interview_type_based_sign_up_limits, 0, fn({interview_type_id, max_sign_up_limit}) -> interview_type_id == interview.interview_type_id && max_sign_up_limit end)
-    if sign_up_evaluation_status.valid? and !is_signup_lesser_than_max_count(interview.id, signup_counts, max_sign_up_limit, is_slot) do
+    if !is_signup_lesser_than_max_count(interview.id, signup_counts, max_sign_up_limit, is_slot) do
       sign_up_evaluation_status = sign_up_evaluation_status |> SignUpEvaluationStatus.add_errors({:signup_count, "More than #{max_sign_up_limit} signups are not allowed"})
     end
     sign_up_evaluation_status
