dde26ddfeb7d8b7c0fc608a0294b736a335831da
Vijay Raghavan Aravamudhan <avijayr - at - gmail - dot - com>
2016-10-28 07:17:18 +0530

    [Vijay] Lock down to specific version of elixir to get the code compiling.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RecruitxBackend.Mixfile do
   def project do
     [app: :recruitx_backend,
      version: "1.0.0",
-     elixir: "~> 1.2.3",
+     elixir: "1.2.6",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
