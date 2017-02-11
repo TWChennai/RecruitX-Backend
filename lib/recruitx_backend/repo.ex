c98e25fa5e3d0d33bca01e2981eddecec9d9a037
Dinesh Balasubramanian <dineshudt17@gmail.com>
2017-02-11 12:02:16 +0530

    [Dinesh] Removed the unused custom repo methods

--- lib/recruitx_backend/repo.ex
+++ lib/recruitx_backend/repo.ex
@@ -4,7 +4,4 @@ defmodule RecruitxBackend.Repo do
 
   Decimal.set_context(%Decimal.Context{Decimal.get_context | precision: 2, rounding: :half_up})
 
-  # TODO: Remove these methods when the codebase has been converted to use Ecto.Multi
-  def custom_insert, do: &(__MODULE__.insert(&1))
-  def custom_update, do: &(__MODULE__.update(&1))
 end
