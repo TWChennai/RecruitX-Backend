1fe7b640d0248c408a5834ae9e39269da0742b45
Dinesh Balasubramanian <dineshudt17@gmail.com>
2017-02-11 12:18:54 +0530

    [Dinesh] Moving the logic to db level

--- web/models/interview.ex
+++ web/models/interview.ex
@@ -246,11 +246,8 @@ defmodule RecruitxBackend.Interview do
     end
   end
 
-  defp is_pass(status_id) do
-    # TODO: Combine into a single db query
-    status = (from i in InterviewStatus, where: i.name == ^PipelineStatus.pass) |> Repo.one
-    !is_nil(status) and status.id == status_id
-  end
+  defp is_pass(nil), do: false
+  defp is_pass(status_id), do: !is_nil((from i in InterviewStatus, where: i.name == ^PipelineStatus.pass and i.id == ^status_id) |> Repo.one)
 
   def delete_successive_interviews_and_panelists(candidate_id, start_time) do
     interviews_to_delete_query = from i in __MODULE__,
